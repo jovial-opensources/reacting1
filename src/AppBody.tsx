eec33d4fdccc6e9699c0899d28fcbf8abeb58ac1
Rishav Bhowmik <bhowmikrishav@gmail.com>
2021-03-09 01:08:40 +0530

    init with header and body

---
+++ src/AppBody.tsx
@@ -0,0 +1,14 @@
+import React, {MouseEventHandler} from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+export function AppBody(){
+    return (
+        <div style={{
+            textAlign: 'left',
+            color: 'royalblue'
+        }}>
+            This the App Body
+        </div>
+    );
+}
---
+++ src/AppHeader.tsx
@@ -0,0 +1,18 @@
+import React, {MouseEventHandler} from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+
+const header_onclick: MouseEventHandler<HTMLHeadingElement> = (e)=>{
+    const x = e.clientX
+    const y = e.clientY
+    console.log({x,y})
+}
+
+export function AppHeader(){
+    return (
+        <h2 color={"yellow"} onClick={header_onclick}>
+            Rishav's App
+        </h2>
+    );
+}
